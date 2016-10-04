59e29716e00420b28b53baa9d41847617da9fdb6
arbennett <bennett.andr@gmail.com>
2016-10-03 21:39:21 -0700

    Adding binary read implementation.
    This rough implementation has not been fully verified, but translated 10 values correctly.

--- metsim/io.py
+++ metsim/io.py
@@ -3,6 +3,7 @@ Handles IO for MetSim
 """
 
 import os
+import struct
 from configparser import ConfigParser
 
 def read_config(fname):
@@ -27,7 +28,28 @@ def read_binary_forcng(fname):
     """
     TODO
     """
-    pass
+    #TODO: There are scale factors that still need to be applied
+    precip = [] # Short unsigned int
+    t_min  = [] # Short int
+    t_max  = [] # Short int
+    wind   = [] # Short int
+    
+    # Pack these for nicer syntax in the loop
+    vars = [precip, t_min, t_max, wind]
+
+    # Data types referred to: 'H' - unsigned short ; 'h' - short
+    types = ['H', 'h', 'h', 'h']
+    with open(fname, 'rb') as f:
+        i = 0
+        while True:
+            bytes = f.read(2)
+            if bytes:
+                # Get correct variable and data type with i, then unpack
+                vars[i].append(struct.unpack(types[i], bytes)[0])
+                i = (i+1)%4
+            else:
+                break
+    return vars
 
 
 def write_to_netcdf(data, fname):
