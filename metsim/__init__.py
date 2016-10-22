4e80f666264159789c1982e9ebe88b4fbfe95b9a
arbennett <bennett.andr@gmail.com>
2016-10-21 18:28:41 -0700

    Added multiple dispatch decorators for io and set up disaggregation functions.

--- metsim/__init__.py
+++ metsim/__init__.py
@@ -9,3 +9,22 @@ METHODS = {
           }
 
 
+def multi(dispatch):
+    def _inner(arg):
+        return _inner.__multi__.get(dispatch(arg), _inner.__multi_error__)(arg)
+
+    _inner.__multi__ = {}
+    _inner.__multi_error__ = lambda x : None 
+    return _inner
+
+
+def method(dispatch, key):
+    def decorate(fn):
+        if key is None:
+            dispatch.__multi_error__ = fn
+        else:
+            dispatch.__multi__[key] = fn
+        return dispatch
+    return decorate
+
+
