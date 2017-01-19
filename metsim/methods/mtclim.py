Fixing calculation of .

metsim/methods/mtclim.py
@@ -47,7 +47,7 @@ def calc_t_air(df: pd.DataFrame, params: dict):
     lapse_rates = [params['t_min_lr'], params['t_max_lr']]
     t_max = df['t_max'] + dZ * lapse_rates[1]
     t_min = np.minimum(df['t_min'] + dZ * lapse_rates[0], t_max-0.5)
-    t_mean = np.mean(t_min + t_max)
+    t_mean = (t_min + t_max)/2
     df['t_day'] = ((t_max - t_mean) * conf.TDAYCOEF) + t_mean
 
 
@@ -138,6 +138,7 @@ def calc_srad_hum(df: pd.DataFrame, sg: dict, params: dict, win_type='boxcar'):
     tdew = df.get('tdew', df['t_min'])
     pva = df.get('hum', svp(tdew))
     pa = atm_pres(df['elev'][0])
+    #pa = atm_pres(5688)
     yday = df['day_of_year'] - 1 
     df['dayl'] = sg['daylength'][yday]
  
@@ -149,6 +150,7 @@ def calc_srad_hum(df: pd.DataFrame, sg: dict, params: dict, win_type='boxcar'):
         tdew_old = np.copy(tdew)
         tdew, pva = sw_hum_iter(df, sg, pa, pva, parray, dtr)
     df['vapor_pressure'] = pva 
+
 
 def sw_hum_iter(df, sg, pa, pva, parray, dtr):
     tt_max0 = sg['tt_max0']

metsim/physics.py
@@ -175,7 +175,6 @@ def solar_geom(elev, lat):
     tt_max0   = np.zeros(dayperyear)
     daylength = np.zeros(dayperyear)
     flat_potrad    = np.zeros(dayperyear) 
-    slope_potrad    = np.zeros(dayperyear) 
     t1 = 1.0 - (conf.LR_STD * elev)/conf.T_STD
     t2 = conf.G_STD / (conf.LR_STD * (conf.R /conf.MA))
     trans = np.power(conf.TBASE, np.power(t1, t2))
@@ -203,7 +202,7 @@ def solar_geom(elev, lat):
         coshss = min(max(-sinegeom / cosegeom, -1), 1)
         hss = np.arccos(coshss)  
         daylength[i] = min(2.0 * hss * conf.SECPERRAD, conf.SEC_PER_DAY)
-        dir_beam_topa = (1368.0 + 45.5 * np.sin((2.0 * np.pi * i / 365.25) + 1.7)) * dt
+        dir_beam_topa = (1368.0+45.5*np.sin((2.0*np.pi*i/conf.DAYS_PER_YEAR)+1.7))*dt
         sum_trans = 0
         sum_flat_potrad= 0
         # Set up angular calculations 
@@ -235,7 +234,6 @@ def solar_geom(elev, lat):
         else:
             tt_max0[i] = 0.
             flat_potrad[i] = 0.
-            slope_potrad[i] = 0.
     tt_max0[dayperyear-1] = tt_max0[dayperyear-2]
     flat_potrad[dayperyear-1] = flat_potrad[dayperyear-2]
     daylength[dayperyear-1] = daylength[dayperyear-2]