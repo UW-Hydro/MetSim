b4ff3623d660a63fa91844114d5e2bd3799ac673
arbennett <bennett.andr@gmail.com>
2017-02-09 12:35:21 -0800

    Adding comments to tests.

--- tests/test_metsim.py
+++ tests/test_metsim.py
@@ -12,17 +12,22 @@ import xarray as xr
 from metsim.metsim import MetSim
 import metsim.constants as const
 
+# Parameters to test over
 in_fmts = ['ascii', 'binary', 'netcdf']
 out_fmts = ['ascii', 'netcdf']
+methods = ['mtclim']
 
+# Where datasets for each input type are found
 data_locations = {'netcdf' : './tests/data/test.nc',
                   'ascii' : './tests/data/ascii/',
                   'binary' : './tests/data/binary/'}
 
+# Dates to run over
 dates = {'netcdf' : (pd.datetime(1950,1,1), pd.datetime(1950,1,31)),
          'binary' : (pd.datetime(1949,1,1), pd.datetime(1949,12,31)),
          'ascii' : (pd.datetime(1949,1,1), pd.datetime(2005,12,31))}
 
+# All values should be in these ranges
 data_ranges = {'temp' : (-50,40),
                'prec' : (0,8),
                'shortwave' : (0,1000),
@@ -31,31 +36,35 @@ data_ranges = {'temp' : (-50,40),
                'vapor_pressure' : (0,1.4),
                'rel_humid' : (0,100)}
 
-methods = ['mtclim']
 
 
 @pytest.fixture(params=in_fmts)
 def in_format(request):
+    """Input formats - see in_fmts`"""
     return request.param
 
 
 @pytest.fixture(params=out_fmts)
 def out_format(request):
+    """Output formats - see `out_fmts`"""
     return request.param
 
 
 @pytest.fixture(params=methods)
 def method(request):
+    """Generation methods - see `methods`"""
     return request.param
 
 
 @pytest.fixture()
 def domain_file():
+    """Domain file containing elevation data"""
     return "./tests/data/domain.nc"
 
 
 @pytest.fixture()
 def test_params(in_format, out_format, method):
+    """Assemble the parameters for each combo"""
     start = dates[in_format][0]
     stop = dates[in_format][1]
     in_vars = ['prec', 't_max', 't_min', 'wind']
@@ -79,6 +88,7 @@ def test_params(in_format, out_format, method):
 
 @pytest.fixture()
 def test_setup(test_params, domain_file):
+    """Tests the setup of the MetSim object"""
     in_fmt = test_params['in_format']
     loc = data_locations[in_fmt]
 
@@ -108,6 +118,7 @@ def test_setup(test_params, domain_file):
 
 
 def test_mtclim(test_setup):
+    """Tests the ability to run successfully"""
     # Here we only test a single grid cell
     loc = test_setup.locations[0]
     strloc = "{}_{}".format(loc[0], loc[1])
@@ -130,6 +141,8 @@ def test_mtclim(test_setup):
 
 
 def test_disaggregation_values():
+    """Tests to make sure values are being generated correctly"""
+    # Set parameters 
     params = {'start' : dates['binary'][0],
               'stop' : dates['binary'][1],
               'in_vars' : ['prec', 't_max', 't_min', 'wind'],
@@ -143,12 +156,21 @@ def test_disaggregation_values():
               't_min_lr' : 0.00065,
               'out_dir' : "./tmp" 
               }   
+    # The location we will test against
     loc = (48.3125,-120.5625)
+
+    # Set up the MetSim object
     ms = MetSim(params)
     ms.load(params['forcings'], params['domain'])
+
+    # Run MetSim and load in the validated data
     out = ms.run([loc])["{}_{}".format(loc[0], loc[1])]
     good = pd.read_table('./tests/data/validated_48.3125_-120.5625', index_col=0)
+
+    # Make sure the data comes out right
     assert type(out) is pd.DataFrame
     for var in out.keys():
+        # Check to make sure each variable is within  
+        # 0.005 validated data
         assert np.allclose(out[var], good[var], 1e-5, 0.005)
 
