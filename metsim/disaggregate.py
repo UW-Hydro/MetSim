ea1e56ccdfa0ce1988c177a2ffcfab71db195890
arbennett <bennett.andr@gmail.com>
2017-07-27 14:45:14 -0700

    Update docs.  Fix rise_time offset.

--- metsim/disaggregate.py
+++ metsim/disaggregate.py
@@ -133,7 +133,7 @@ def set_min_max_hour(disagg_rad: pd.Series, n_days: int,
     rise_times = np.where(diff_mask > 0)[0] * ts
     set_times = np.where(diff_mask < 0)[0] * ts
     t_t_max = (params['tmax_daylength_fraction'] * (set_times - rise_times) +
-               rise_times)
+               rise_times) + ts
     t_t_min = rise_times
     return t_t_min, t_t_max
 
