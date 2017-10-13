6c01cdd4d287d7298b7d6c9050956fd710bb2201
kwhitney727 <kristen.m.whitney@gmail.com>
2017-10-13 13:53:11 -0700

    Merged with feature/wind_output branch and cleaned up wrapper script.

--- wrap_metsim_over_time.py
+++ wrap_metsim_over_time.py
@@ -3,7 +3,6 @@
 import xarray as xr
 from metsim import MetSim
 import os.path
-import numpy as np
 import sys, getopt
 import pandas as pd
 from collections import OrderedDict
@@ -13,7 +12,7 @@ import datetime
 def main():
     computation_start = datetime.datetime.now()
     infile_directory = ''
-    spintup = ''
+    spinup = ''
     domainfile = ''
     statefile_initial = ''
     infile_prefix = ''
@@ -22,24 +21,28 @@ def main():
     startdate = ''
     enddate = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hi:p:d:s:f:o:u:a:b:",
-                                    ["infile_directory=", "spinup="
-                                    "domainfile=","statefile_initial=",
-                                    "infile_prefix=","outfile_prefix=",
-                                    "outfile_directory=","startdate",
-                                    "enddate="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:p:d:s:f:o:u:a:b:",
+                                                 ["infile_directory=", "spinup="
+                                                  "domainfile=",
+                                                  "statefile_initial=",
+                                                  "infile_prefix=",
+                                                  "outfile_prefix=",
+                                                  "outfile_directory=",
+                                                  "startdate",
+                                                  "enddate="])
     except getopt.GetoptError:
         print(sys.argv[0], '-i <infile_directory> -p <spinup>'
-                            '-d <domainfile> -f <infile_prefix> '
-                            '-s <statefile_initial> -o <outfile_prefix> -u '
-                            '<outfile_directory> -a <startdate> -b <enddate>')
+                           '-d <domainfile> -f <infile_prefix> '
+                           '-s <statefile_initial> -o <outfile_prefix> -u '
+                           '<outfile_directory> -a <startdate> -b <enddate>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print(sys.argv[0], ' -i <infile_directory> -p <spinup>'
-                                '-d <domainfile> -s <statefile_initial> '
-                                '-f <infile_prefix> -o <outfile_prefix> -u '
-                                '<outfile_directory> -a <startdate> -b <enddate>')
+            print(sys.argv[0], ' -i <infile_directory> -p <spinup> '
+                               '-d <domainfile> -s <statefile_initial> '
+                               '-f <infile_prefix> -o <outfile_prefix> -u '
+                               '<outfile_directory> -a <startdate>'
+                               ' -b <enddate>')
             sys.exit()
         elif opt in ("-i", "--infile_directory"):
             infile_directory = arg
@@ -60,7 +63,7 @@ def main():
         elif opt in ("-b", "--enddate"):
             enddate = arg
 
-    #infiles = os.listdir(infile_directory)
+    # infiles = os.listdir(infile_directory)
     outfile_directory = outfile_directory + '/'
     state = xr.open_dataset(statefile_initial)
     domain = xr.open_dataset(domainfile)
@@ -75,13 +78,14 @@ def main():
     last_day = int(last_day)
     last_year = int(last_year)
 
-    forcing_file = infile_directory + '/' + infile_prefix + "_%04d%02d" % (first_year,first_month) + '.nc'
+    forcing_file = infile_directory + '/' + \
+                   infile_prefix + "%04d%02d" % (first_year, first_month) + \
+                   '.nc'
     forcing_intial = xr.open_dataset(forcing_file)
-    outfile_prefix_month_1 = "%s%04d%02d" % (outfile_prefix,first_year,first_month)
+    outfile_prefix_month_1 = "%s%04d%02d" % (outfile_prefix, first_year, first_month)
 
     year = first_year
     date_i = 0
-
 
     params = {
         "method": 'mtclim',
@@ -98,19 +102,19 @@ def main():
         "annual": False,
         'forcing': forcing_file,
         'state_vars': OrderedDict({'prec': 'prec',
-                         't_max': 't_max',
-                         't_min': 't_min',
-                          'swe': 'swe'}),
+                                   't_max': 't_max',
+                                   't_min': 't_min',
+                                   'swe': 'swe'}),
         'forcing_vars': OrderedDict({'Prec': 'prec',
-                           'Tmax': 't_max',
-                           'Tmin': 't_min',
-                           'wind': 'wind'}),
+                                     'Tmax': 't_max',
+                                     'Tmin': 't_min',
+                                     'wind': 'wind'}),
         'domain_vars': OrderedDict({'lat': 'lat',
-                          'lon': 'lon',
-                          'mask': 'mask',
-                          'elev': 'elev',
-                          't_pk': 't_pk',
-                          'dur': 'dur'})
+                                    'lon': 'lon',
+                                    'mask': 'mask',
+                                    'elev': 'elev',
+                                    't_pk': 't_pk',
+                                    'dur': 'dur'})
     }
 
     forcing_outfile_tmp = outfile_directory + 'forcing_tmp' + '_total.nc'
@@ -124,13 +128,17 @@ def main():
             month_current = startdate.month
             year_current = startdate.year
             if month_current < 12:
-                enddate = pd.to_datetime(("%04d%02d%02d" % (year_current,month_current + 1,1)),format='%Y%m%d') - pd.Timedelta('1 days')
+                enddate = pd.to_datetime(("%04d%02d%02d" % (year_current,
+                                                            month_current +
+                                                            1, 1)), format='%Y%m%d') - pd.Timedelta('1 days')
             else:
-                enddate = pd.to_datetime(("%04d%02d%02d" % (year_current + 1,1,1)),format='%Y%m%d') - pd.Timedelta('1 days')
+                enddate = pd.to_datetime(("%04d%02d%02d" % (year_current +
+                                                            1, 1, 1)), format='%Y%m%d') - pd.Timedelta('1 days')
 
             if year == first_year and month_i == 0:
 
-                # delete any previous temporary state files created with ms.run()
+                # delete any previous temporary state files created with
+                # ms.run()
                 try:
                     os.remove(outfile_directory + 'state_tmp.nc')
                 except:
@@ -138,21 +146,26 @@ def main():
                 params["forcing"] = forcing_file
                 params["start"] = startdate
                 params["stop"] = enddate
-                print('Running forcings from start and stop dates:',startdate,enddate)
+                print('Running forcings from start and stop dates:', startdate,
+                      enddate)
                 ms = MetSim(params)
                 ms.run()
 
-                # delete any previous temporary state files created in wrapper script and save the current
+                # delete any previous temporary state files created in wrapper
+                # script and save the current
                 try:
                     os.remove(state_outfile)
                 except:
                     pass
                 ms.state.to_netcdf(state_outfile)
             else:
-                forcing_filename = (infile_prefix + "_%04d%02d" % (year_current,month_current) + '.nc')
+                forcing_filename = (infile_prefix +
+                                    "%04d%02d" % (year_current,
+                                                   month_current) + '.nc')
                 infile = (infile_directory + forcing_filename)
 
-                # delete any previous temporary state files created with ms.run()
+                # delete any previous temporary state files created with
+                # ms.run()
                 try:
                     os.remove(outfile_directory + 'state_tmp.nc')
                 except:
@@ -164,10 +177,12 @@ def main():
                 params['stop'] = enddate
                 params['forcing'] = infile
                 ms = MetSim(params)
-                print('Running forcings from start and stop dates:',startdate,enddate)
+                print('Running forcings from start and stop dates:', startdate,
+                      enddate)
                 ms.run()
 
-                # delete any previous temporary state files created in wrapper script and save the current
+                # delete any previous temporary state files created in wrapper
+                # script and save the current
                 try:
                     os.remove(state_outfile)
                 except:
@@ -183,7 +198,8 @@ def main():
                 if month_i == 0:
                     forcing_concat = forcing_current
                 else:
-                    forcing_concat = xr.concat([forcing_concat,forcing_current], dim='time')
+                    forcing_concat = xr.concat([forcing_concat,
+                                                forcing_current], dim='time')
 
             # delete the current month's forcing files
             try:
@@ -192,7 +208,8 @@ def main():
                 pass
 
             date_i += 1
-            print('Completed forcings from start and stop dates:', startdate, enddate)
+            print('Completed forcings from start and stop dates:',
+                  startdate, enddate)
         if spinup:
             # save the state file at the end of spinup
             print('Saving spinup month 12 state file')
@@ -211,13 +228,14 @@ def main():
             params['state'] = state_spinup_end_file
         else:
             # output concatenated dataset
-            outfile = (outfile_directory + '/' + outfile_prefix + "%04d_" % (year) + 'total.nc')
-            print('Saving yearly datasets to outfile:',outfile)
+            outfile = (outfile_directory + '/' + outfile_prefix +
+                       "%04d_" % (year) + 'total.nc')
+            print('Saving yearly datasets to outfile:', outfile)
             try:
                 os.remove(outfile)
             except:
                 pass
-            forcing_concat.to_netcdf(outfile ,engine='scipy')
+            forcing_concat.to_netcdf(outfile, engine='scipy')
 
             year = year + 1
 
@@ -230,8 +248,8 @@ def main():
     forcing_intial.close()
     domain.close()
     computation_end = datetime.datetime.now()
-    computation_total =  computation_end - computation_start
-    print('Total computation time:',computation_total)
+    computation_total = computation_end - computation_start
+    print('Total computation time:', computation_total)
 
 
 if __name__ == "__main__":
