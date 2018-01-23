ef8f2743af0e0c895278ac822a40ee7aba126353
arbennett <bennett.andr@gmail.com>
2018-01-22 23:21:05 -0800

    Refactor mtclim into individual variable functions.

--- metsim/methods/mtclim.py
+++ metsim/methods/mtclim.py
@@ -19,72 +19,73 @@ MTCLIM
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
-import pandas as pd
 
 import metsim.constants as cnst
 from metsim.physics import svp, calc_pet, atm_pres
 
 
-def run(df: pd.DataFrame, params: dict, sg: dict):
-    """
-    Run all of the mtclim df generation
-
-    Parameters
-    ----------
-    df: pd.DataFrame
-        The daily dfs given from input
-    params: dict
-        Dictionary of parameters from a MetSim object
-    solar_geom: dict
-        Solar geometry of the site
-
-    Returns
-    -------
-    df:
-        Dataframe of daily or subdaily dfs
-    """
-    t_mean = (df['t_min'] + df['t_max']) / 2
-    df['t_day'] = (
-            (df['t_max'] - t_mean) * params['tday_coef']) + t_mean
-    b = cnst.B0 + cnst.B1 * np.exp(-cnst.B2 * df['smoothed_dtr'])
-    df['tfmax'] = (
-            1.0 - 0.9 * np.exp(-b * np.power(df['dtr'], cnst.C)))
-    inds = np.array(df['prec'] > params['sw_prec_thresh'])
-    df['tfmax'][inds] *= params['rain_scalar']
-
-    tdew = df.get('tdew', df['t_min'])
-    df['vapor_pressure'] = df.get('vapor_pressure', svp(tdew.values))
-    pa = atm_pres(params['elev'], params['lapse_rate'])
-    yday = df.index.dayofyear - 1
-    df['dayl'] = sg['daylength'][yday]
-
-    # Calculation of tdew and shortwave. tdew is iterated on until
-    # it converges sufficiently
-    tdew_old = tdew
-    err = 500
-    while(err > params['tdew_tol']):
-        tdew_old = np.copy(tdew)
-        yday = df.index.dayofyear - 1
-        t_tmax = np.maximum(sg['tt_max0'][yday]
-                            + (cnst.ABASE * df['vapor_pressure']), 0.0001)
-        df['shortwave'] = sg['potrad'][yday] * t_tmax * df['tfmax']
-        # Compute PET using SW radiation estimate, and update Tdew, pva **
-        pet = calc_pet(df['shortwave'].values, df['t_day'].values,
-                       df['dayl'].values, pa)
-        # Calculate ratio (PET/effann_prcp) and correct the dewpoint
-        parray = df['seasonal_prec'] / cnst.MM_PER_CM
-        ratio = pet / parray.where(parray > 8.0, 8.0)
-        df['pet'] = pet * cnst.MM_PER_CM
-        tmink = df['t_min'] + cnst.KELVIN
-        tdew = (tmink * (-0.127 + 1.121 * (1.003 - 1.444 * ratio
-                + 12.312 * np.power(ratio, 2) - 32.766 * np.power(ratio, 3))
-                + 0.0006 * df['dtr']) - cnst.KELVIN)
-        err = np.sqrt(np.mean((tdew-tdew_old)**2))
-    df['vapor_pressure'] = svp(tdew.values)
-
-    # Calculate cloud effect
-    if (params['lw_cloud'].upper() == 'CLOUD_DEARDORFF'):
-        df['tskc'] = (1. - df['tfmax'])
-    else:
-        df['tskc'] = np.sqrt((1. - df['tfmax']) / 0.65)
+def run(df, params, sg):
+    df['t_day'] = t_day(df['t_min'].values, df['t_max'].values, params)
+    df['tfmax'] = tfmax(df['dtr'].values, df['smoothed_dtr'].values,
+                        df['prec'].values, params)
+    df['tskc'] = tskc(df['tfmax'].values, params)
+    yday = df.index.dayofyear.values - 1
+    df['dayl'] = dayl(yday, sg)
+    vp_temp = vapor_pressure(df['t_min'].values)
+    sw_temp = shortwave(yday, df['tfmax'].values, vp_temp, sg)
+    pet_temp = pet(sw_temp, df['t_day'].values, df['dayl'].values, params)
+    df['tdew'] = tdew(pet_temp, df['t_min'].values, df['seasonal_prec'].values,
+                      df['dtr'].values)
+    df['vapor_pressure'] = vapor_pressure(df['tdew'].values)
+    df['shortwave'] = shortwave(yday, df['tfmax'].values,
+                                df['vapor_pressure'].values, sg)
+    df['pet'] = pet(df['shortwave'].values, df['t_day'].values,
+                    df['dayl'].values, params)
     return df
+
+
+def t_day(t_min, t_max, params):
+    t_mean = (t_min + t_max) / 2
+    return ((t_max - t_mean) * params['tday_coef']) + t_mean
+
+
+def tfmax(dtr, sm_dtr, prec, params):
+    b = cnst.B0 + cnst.B1 * np.exp(-cnst.B2 * sm_dtr)
+    tfmax = 1.0 - 0.9 * np.exp(-b * np.power(dtr, cnst.C))
+    inds = np.array(prec > params['sw_prec_thresh'])
+    tfmax[inds] *= params['rain_scalar']
+    return tfmax
+
+
+def dayl(yday, sg):
+    return sg['daylength'][yday]
+
+
+def pet(shortwave, t_day, dayl, params):
+    pa = atm_pres(params['elev'], params['lapse_rate'])
+    return calc_pet(shortwave, t_day, dayl, pa) * cnst.MM_PER_CM
+
+
+def tdew(pet, t_min, seasonal_prec, dtr):
+    parray = seasonal_prec < 80.0
+    seasonal_prec[parray] = 80.0
+    ratio = pet / seasonal_prec
+    return ((t_min + cnst.KELVIN) * (-0.127 + 1.121 * (1.003 - 1.444 * ratio
+            + 12.312 * np.power(ratio, 2) - 32.766 * np.power(ratio, 3))
+            + 0.0006 * dtr) - cnst.KELVIN)
+
+
+def vapor_pressure(tdew):
+    return svp(tdew)
+
+
+def shortwave(yday, tfmax, vapor_pressure, sg):
+    t_tmax = np.maximum(sg['tt_max0'][yday]
+                        + (cnst.ABASE * vapor_pressure), 0.0001)
+    return sg['potrad'][yday] * t_tmax * tfmax
+
+
+def tskc(tfmax, params):
+    if (params['lw_cloud'].upper() == 'CLOUD_DEARDORFF'):
+        return 1. - tfmax
+    return np.sqrt((1. - tfmax) / 0.65)
