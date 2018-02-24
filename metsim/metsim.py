5bf158c51b0a85d45c31c1b906f39aba04f9ab27
Andrew Bennett <bennett.andr@gmail.com>
2018-02-23 16:38:53 -0800

    Fix/time metadata (#124)
    * Adding documentation on longwave calculations
    
    * Add references for longwave methods
    
    * Update time attributes when using UTC time.
    
    * Move definition of time attributes for locality.

--- metsim/metsim.py
+++ metsim/metsim.py
@@ -86,14 +86,11 @@ attrs = {'pet': {'units': 'mm timestep-1', 'long_name': 'potential evaporation',
                        'standard_name': 'relative_humidity'},
          'spec_humid': {'units': '', 'long_name': 'specific humidity',
                         'standard_name': 'specific_humidity'},
-         'time': {'long_name': 'local time at grid location',
-                  'standard_name': 'local_time'},
          '_global': {'conventions': '1.6', 'title': 'Output from MetSim',
                      'institution': 'University of Washington',
                      'source': 'metsim.py',
                      'history': 'Created: {0} by {1}'.format(now, user),
                      'references': references,
-                     'documentation': 'Times given are local to their locations.',
                      'comment': 'no comment at this time'}}
 
 attrs = {k: OrderedDict(v) for k, v in attrs.items()}
@@ -225,6 +222,12 @@ class MetSim(object):
         logger.info('state stop {}'.format(self.params['state_stop']))
 
         logger.info('calendar {}'.format(self.params['calendar']))
+        if self.params['utc_offset']:
+            attrs['time'] = {'long_name': 'UTC time',
+                             'standard_name': 'utc_time'}
+        else:
+            attrs['time'] = {'long_name': 'local time at grid location',
+                             'standard_name': 'local_time'}
 
     def load_inputs(self, close=True):
         self.domain = self.domain.load()
