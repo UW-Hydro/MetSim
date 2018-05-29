8264cbdb4189777a8c922c3d80bf69414761eadb
arbennett <bennett.andr@gmail.com>
2018-05-29 11:05:43 -0700

    Update to version 1.1

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ except:
     from distutils.core import setup
 
 setup(name='metsim',
-      version='1.0.0',
+      version='1.1.0',
       description='Meteorology Simulator',
       url='https://github.com/UW-Hydro/MetSim',
-      download_url='https://github.com/UW-Hydro/MetSim/archive/v1.0.tar.gz',
+      download_url='https://github.com/UW-Hydro/MetSim/archive/v1.1.tar.gz',
       author='Andrew Bennett',
       author_email='bennett.andr@gmail.com',
       packages=['metsim', 'metsim.methods'],
