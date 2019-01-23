Fix off by one error in temperature disaggregation

metsim/disaggregate.py
@@ -162,8 +162,8 @@ def set_min_max_hour(tiny_rad_fract: np.array, yday: np.array, n_days: int,
 
     # time of maximum and minimum temperature calculated thusly
     t_t_max = (params['tmax_daylength_fraction'] * (set_times - rise_times) +
-               rise_times) + ts
-    t_t_min = rise_times
+               rise_times)
+    t_t_min = rise_times -ts
     return t_t_min, t_t_max
 
 